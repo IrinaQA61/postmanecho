124b62fd2879c275d4b02c5d5df1498b58034ba8
Neto test <irina.plotnikova72@mail.ru>
2023-07-19 17:41:57 +0500

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+package ru.netology.rest;
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hi"))
+        ;
+    }
+}
+
+
